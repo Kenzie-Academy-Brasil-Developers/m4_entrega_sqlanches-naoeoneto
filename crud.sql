9872cf5a3e3476a904a5c185c46068feb821b867
naoeoneto <abdpneto@gmail.com>
2022-12-01 21:20:37 -0300

    feat: finish crud

--- crud.sql
+++ crud.sql
@@ -4,31 +4,96 @@
 -- Criação
 
 -- 1)
-
-
+INSERT INTO clientes
+	(nome, lealdade)
+VALUES
+	('Georgia', 0);
 
 -- 2)
-
-
+INSERT INTO pedidos
+	(status, cliente_id)
+VALUES
+	('Recebido', 6);
 
 -- 3)
-
-
+INSERT INTO produtos_pedidos
+	(pedido_id, produto_id)
+VALUES
+	(6, (SELECT id FROM produtos WHERE nome = 'Big Serial')),
+	(6, (SELECT id FROM produtos WHERE nome = 'Varchapa')),
+	(6, (SELECT id FROM produtos WHERE nome = 'Fritas')),
+	(6, (SELECT id FROM produtos WHERE nome = 'Coca-Cola')),
+	(6, (SELECT id FROM produtos WHERE nome = 'Coca-Cola'));
 
 -- Leitura
 
 -- 1)
-
-
+SELECT
+	c.id,
+ 	c.nome,
+ 	c.lealdade,
+ 	pe.id,
+ 	pe.status,
+ 	pe.cliente_id,
+ 	pr.id,
+ 	pr.nome,
+ 	pr.tipo,
+ 	pr.preço,
+ 	pr.pts_de_lealdade
+FROM 
+	clientes c 
+JOIN
+	pedidos pe 
+ON
+	c.id = pe.cliente_id
+JOIN 
+	produtos_pedidos pp 
+ON
+	pp.pedido_id = 6
+JOIN
+	produtos pr
+ON
+	pr.id = pp.produto_id 
+WHERE
+	c.id = pp.pedido_id;
 
 -- Atualização
 
 -- 1)
-
-
+UPDATE
+	clientes 
+SET
+	lealdade = (
+	SELECT
+		SUM(pr.pts_de_lealdade) 
+	FROM
+		produtos pr
+	JOIN
+		produtos_pedidos pp 
+	ON
+		pr.id = pp.produto_id 
+	WHERE
+		pp.pedido_id = 6)
+WHERE
+	id = (
+	SELECT
+		id
+	FROM
+		clientes
+	WHERE
+		nome = 'Georgia');
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM 
+	clientes
+WHERE
+	id = ( 
+	SELECT
+		id
+	FROM
+		clientes
+	WHERE
+		nome = 'Marcelo');
 
--- selects.sql
+++ selects.sql
@@ -5,14 +5,7 @@
 
 
 -- 2)
-SELECT
-	pp.pedido_id
-FROM
-	produtos_pedidos pp
-WHERE
-	produto_id = (
-	SELECT id FROM produtos WHERE nome = 'Fritas'
-	);
+
 
 -- 3)
 
